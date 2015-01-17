b09561ee65f3b48f8fa0671509ef4023dadb90f4
Shane da Silva <shane@dasilva.io>
2015-01-17 01:26:03 -0800

    Split tmux plugin into separate files for bash/zsh
    Previously, we tried to have a single file that defined the `tmux`
    helper which worked in both bash and zsh.
    
    Unfortunately, this just became too impractical. I'm pulled them apart
    into their separate scripts to restore some sanity.

--- plugins/tmux/plugin.zsh
+++ plugins/tmux/plugin.zsh
@@ -1,13 +1,13 @@
 alias t=tmux
 
-tmux() {
+tmux () {
   # Symlink auth socket so that when we reconnect we can point to the newer
   # socket by just updating the symlink. Saves us from having to manually reset
   # SSH_AUTH_SOCK using `ssh-reagent` or similar.
   SOCK_SYMLINK=~/.ssh/tmux_ssh_auth_sock
   if [ -r "$SSH_AUTH_SOCK" ]; then
     rm -f $SOCK_SYMLINK && ln -s $SSH_AUTH_SOCK $SOCK_SYMLINK
-    tmux_env="SSH_AUTH_SOCK=$SOCK_SYMLINK "
+    tmux_env="SSH_AUTH_SOCK=$SOCK_SYMLINK"
   else
     tmux_env=""
   fi
@@ -18,8 +18,11 @@ tmux() {
 
     # Attach to session with the current directory name if one exists,
     # otherwise automatically create a session with the current directory name
-    env $tmux_env `which tmux` new -A -s $session_name
+    tmux_cmd="new -A -s $session_name"
   else
-    env $tmux_env `which tmux` "$@"
+    # Otherwise just forward along the arguments given
+    tmux_cmd="$@"
   fi
+
+  env ${(z)tmux_env} `whence -p tmux` ${(z)tmux_cmd}
 }
