6e7d4644bf8dfd22be8643c42c5bffee3f078554
Shane da Silva <shane@dasilva.io>
2015-01-29 22:19:16 -0800

    Don't require virtualenv to install via pip
    This was added earlier in my understanding of the Python ecosystem. I no
    longer need this protection in place to avoid polluting the global
    package space.

--- plugins/pip/plugin.sh
+++ plugins/pip/plugin.sh
@@ -2,9 +2,5 @@
 shell=`current_shell`
 if [[ "$shell" =~ (ba|z)sh ]]; then
   which pip >/dev/null 2>&1 && \
-    eval `PIP_REQUIRE_VIRTUALENV=false pip completion --$shell`
+    eval `pip completion --$shell`
 fi
-
-# Require virtualenv when installing packages with pip.
-# Temporarily override by prefixing command with PIP_REQUIRE_VIRTUALENV=false
-export PIP_REQUIRE_VIRTUALENV=true
