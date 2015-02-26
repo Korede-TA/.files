0e1db87a05d8ca5a548ef0fd413b7a4c5486e28e
Shane da Silva <shane.dasilva@brigade.com>
2015-02-26 07:39:26 +0000

    Run pip autocompletion script only in bash
    This was displaying an error in zsh, and zsh has a different completion
    system anyway.

---
+++ plugins/pip/plugin.bash
@@ -0,0 +1,2 @@
+# Enable bash autocompletion for pip
+which pip >/dev/null 2>&1 && eval "$(pip completion --bash)"
--- plugins/pip/plugin.sh
+++
@@ -1,6 +0,0 @@
-# Enable autocompletion for pip (only available for bash/zsh)
-shell=`current_shell`
-if [[ "$shell" =~ (ba|z)sh ]]; then
-  which pip >/dev/null 2>&1 && \
-    eval `pip completion --$shell`
-fi
