f6983ef66998d8452a708e8137d644a44a3ae95f
Shane da Silva <shane@dasilva.io>
2016-07-22 18:56:23 -0700

    Remove unnecessary alfred link cask command
    This command no longer exists now that Homebrew Cask has been merged
    with the Homebrew core project.

--- plugins/alfred/setup.sh
+++ plugins/alfred/setup.sh
@@ -3,12 +3,3 @@ setup() {
     cask alfred
   fi
 }
-
-install() {
-  if [ `uname` = Darwin ]; then
-    # Add cask room to Alfred's index of paths to search for applications
-    if [[ `brew cask alfred status` =~ "not linked" ]]; then
-      brew cask alfred link
-    fi
-  fi
-}
