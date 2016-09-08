3d6e58fd8c5822f8dc2c148e792e6ee7ea79a832
Shane da Silva <shane@dasilva.io>
2016-09-07 23:51:53 -0700

    Enable accessibility for install via SSH
    The fix in 97c40aa was incorrect. The problem wasn't anything to do with
    Remote Desktop Agent–it was the sshd-keygen-wrapper that needed access.

--- plugins/keyboard/setup.sh
+++ plugins/keyboard/setup.sh
@@ -1,6 +1,9 @@
 install() {
   if [ `uname` = Darwin ]; then
-    enable_accessibility 'com.apple.RemoteDesktopAgent'
+    # Enable accessibility for installs via remote SSH connection
+    enable_accessibility '/usr/libexec/sshd-keygen-wrapper'
+
+    # Enable accessibility for installs via typical terminals
     enable_accessibility 'com.apple.Terminal'
     enable_accessibility 'com.googlecode.iterm2'
 
