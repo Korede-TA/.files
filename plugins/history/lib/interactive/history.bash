414023c1ef73ed3aa21a0dd352a97aa1af8c1bb1
Shane da Silva <shane@dasilva.io>
2017-05-25 23:53:46 -0700

    Ignore history commands when prefixed with whitespace
    When working with sensitive information in command arguments, it's nice
    to not have to worry about erasing the relevant history line. Make this
    easy by adding support for ignoring commands prefixed with whitespace.

--- plugins/history/lib/interactive/history.bash
+++ plugins/history/lib/interactive/history.bash
@@ -4,3 +4,8 @@ export PROMPT_COMMAND="$PROMPT_COMMAND; history -a"
 
 # Always append to bash history file (don't overwrite)
 shopt -s histappend
+
+# Don't remember commands with a leading space
+# (useful for hiding sensitive arguments like passwords).
+# Also ignore duplicate commands
+export HISTCONTROL=ignorespace:ignoredups
--- plugins/history/lib/interactive/history.zsh
+++ plugins/history/lib/interactive/history.zsh
@@ -1,6 +1,10 @@
 # Append command to history file once executed
 # (so we don't lose history if disconnected)
-setopt inc_append_history
+setopt INC_APPEND_HISTORY
 
 # Ignore duplicate entries
-setopt hist_ignore_all_dups
+setopt HIST_IGNORE_ALL_DUPS
+
+# Don't remember commands with a leading space
+# (useful for hiding sensitive arguments like passwords)
+setopt HIST_IGNORE_SPACE
